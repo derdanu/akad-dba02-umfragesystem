8c39ce3b83f36789940f404b7996756476c14652
Daniel Falkner <bmw@740i.de>
2013-08-22 10:27:26 +0200

    bootstrap ist nun default index.

--- classes/Controller/Index.php
+++ classes/Controller/Index.php
@@ -20,7 +20,7 @@ class Index {
 		$conf = \Config::getInstance();
 		
 		$view = new \View();
-		$view->setTemplate('bootstrap');
+		$view->setTemplate('index');
 		$view->assign('controller', 'Hello vom Controller ' . get_class($this));
 		$view->assign('config', 'Hello vom Controller und Config ' . $conf->website);
 		$view->display();
