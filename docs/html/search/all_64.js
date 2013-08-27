4ccca4c32152904a7cc46018cbc62109692c5cda
Daniel Falkner <bmw@740i.de>
2013-08-27 17:29:51 +0200

    Doku

--- docs/html/search/all_64.js
+++ docs/html/search/all_64.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['database',['Database',['../class_database.html',1,'']]],
   ['destroy',['destroy',['../class_session.html#a5dde74b6fa44649e5b73cb1096930dd4',1,'Session']]],
   ['display',['display',['../class_view.html#a0924100c5e1690a4150bb1c446859e07',1,'View']]]
 ];
--- docs/html/search/functions_5f.js
+++ docs/html/search/functions_5f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5f_5fconstruct',['__construct',['../class_controller_1_1_admin_1_1_base.html#a7e433734833c21c222186860f4cd8ce5',1,'Controller\Admin\Base\__construct()'],['../class_controller_1_1_survey.html#a96fc5091aa3b11390854987ecf667f15',1,'Controller\Survey\__construct()'],['../class_front_controller.html#ad0793ef177eb6e77a45a476567b531b2',1,'FrontController\__construct()']]]
+  ['_5f_5fconstruct',['__construct',['../class_controller_1_1_admin_1_1_base.html#a7e433734833c21c222186860f4cd8ce5',1,'Controller\Admin\Base\__construct()'],['../class_controller_1_1_survey.html#a96fc5091aa3b11390854987ecf667f15',1,'Controller\Survey\__construct()'],['../class_database.html#a2852f635197e76a838486e64e00aac9f',1,'Database\__construct()'],['../class_front_controller.html#ad0793ef177eb6e77a45a476567b531b2',1,'FrontController\__construct()']]]
 ];
