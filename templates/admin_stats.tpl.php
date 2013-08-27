71cc332bcfe740cc0aee01a82b401298382feddc
Daniel Falkner <bmw@740i.de>
2013-08-27 08:52:44 +0200

    EyeCandy

--- templates/admin_stats.tpl.php
+++ templates/admin_stats.tpl.php
@@ -1,5 +1,13 @@
 		<h1>Umfragen Statistik</h1>
-		<p><?php print $this->view['stats'][survey_cnt]; ?> Umfragen</p>
-		<p><?php print $this->view['stats'][record_cnt]; ?> Antworten</p>
-		
+
+		<ul class="list-group">
+  			<li class="list-group-item">
+    			<span class="badge"><?php print $this->view['stats'][survey_cnt]; ?></span>
+    			Umfragen
+  			</li>
+  			<li class="list-group-item">
+    			<span class="badge"><?php print $this->view['stats'][record_cnt]; ?></span>
+    			Antworten
+  			</li>
+		</ul>
 		
