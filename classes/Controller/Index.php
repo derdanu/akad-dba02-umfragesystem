b6cdcca056b21f711fc333887670594622b8b412
Daniel Falkner <bmw@740i.de>
2013-08-27 17:29:24 +0200

    Konfigwerte entfernt

--- classes/Controller/Index.php
+++ classes/Controller/Index.php
@@ -17,12 +17,8 @@ class Index {
 	 */
 	public function Index_Action() {
 		
-		$conf = \Config::getInstance();
-		
 		$view = new \View();
 		$view->setTemplate('index');
-		$view->assign('controller', 'Hello vom Controller ' . get_class($this));
-		$view->assign('config', 'Hello vom Controller und Config ' . $conf->website);
 		$view->display();
 		
 	}	
