0cb3f57c8daa22cc656c2dca3f7163d3b4fbb9de
Daniel Falkner <bmw@740i.de>
2013-08-27 13:02:50 +0200

    Formatierung und Debug off

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -3,6 +3,7 @@
         <h1>Umfragensystem</h1>
         <p>Zum Teilnehmen an einer Umfrage klicken Sie bitte auf Umfragen</p>
         <p>Haben Sie eine Benutzerkennung so können Sie sich oben rechts anmelden</p>
+        <br /><br />
         <p>
           <a class="btn btn-lg btn-primary" href="?controller=Survey">zu den Umfragen &raquo;</a>
         </p>
