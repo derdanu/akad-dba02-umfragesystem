10f7398b68cc3d7357fe029f2af03a80d6d75c48
Daniel Falkner <bmw@740i.de>
2013-08-27 15:45:02 +0200

    Testanpassung

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,8 +1,8 @@
 
       <div class="jumbotron">
-        <h1>Umfragensystem</h1>
-        <p>Zum Teilnehmen an einer Umfrage klicken Sie bitte auf Umfragen</p>
-        <p>Haben Sie eine Benutzerkennung so können Sie sich oben rechts anmelden</p>
+        <h1>Umfrage System</h1>
+        <p>Zum Teilnehmen an einer Umfrage klicken Sie bitte auf Umfragen.</p>
+        <p>Haben Sie eine Benutzerkennung? Einfach oben rechts anmelden.</p>
         <br /><br />
         <p>
           <a class="btn btn-lg btn-primary" href="?controller=Survey">zu den Umfragen &raquo;</a>
