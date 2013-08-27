90c64f2f1c4cdc5ee866c4f1498fbe6ae37e8d85
Daniel Falkner <bmw@740i.de>
2013-08-27 08:52:35 +0200

    Modal Vorbereitung

--- templates/admin_surveys.tpl.php
+++ templates/admin_surveys.tpl.php
@@ -9,4 +9,23 @@
 			}
 			?>
 		</table>
-		<button type="button" class="btn btn-success">Hinzufügen</button>
+		<a data-toggle="modal" href="#addSurveyModal" class="btn btn-success">Hinzufügen</a>
+
+	  <!-- Modal -->
+	  <div class="modal" id="addSurveyModal">
+	    <div class="modal-dialog">
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+	          <h4 class="modal-title">Neue Umfrage hinzufügen</h4>
+	        </div>
+	        <div class="modal-body">
+	          ...
+	        </div>
+	        <div class="modal-footer">
+	          <a href="#" class="btn" data-dismiss="modal">Verwerfen</a>
+	          <a href="#" class="btn btn-primary">Speichern</a>
+	        </div>
+	      </div><!-- /.modal-content -->
+	    </div><!-- /.modal-dialog -->
+	  </div><!-- /.modal -->
