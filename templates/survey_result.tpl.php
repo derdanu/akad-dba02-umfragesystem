bc783c2f0372f7a23fe89a1f84e642092d14d322
Daniel Falkner <bmw@740i.de>
2013-08-27 12:55:23 +0200

    Eye Candy

--- templates/survey_result.tpl.php
+++ templates/survey_result.tpl.php
@@ -3,13 +3,17 @@
 			<div class="panel-heading">
 			<h3 class="panel-title"><?php print $this->view['survey_name']; ?></h3>
 			</div>
-			<table class="table table-bordered">
+			<table class="table table-bordered table-survey">
 			<?php
 			foreach ($this->view['survey_result'] as $key=>$value) {
-				$width = $key * 5;
-				print "<tr><td>$value</td><td><div style='background-color:grey; width:{$width}px; padding-left:10px;'>$key %</div></td></tr>";	
+				print   "<tr><td>$value</td>" .
+						"<td>" .
+						"<div style='background-color:#428BCA; width:{$key}%; padding-left:10px; border-radius: 18px;'>$key %</div>" .
+						"</td>" .
+						"</tr>";	
 			}
 			?>
+			
 			</table>
 			</div>
 		</div>
