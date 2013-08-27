731d6454b08f43251e99e3731a3a324825effc74
Daniel Falkner <bmw@740i.de>
2013-08-27 08:30:26 +0200

    Last Login

--- templates/admin_user.tpl.php
+++ templates/admin_user.tpl.php
@@ -1,13 +1,14 @@
 		<h1>Benutzer Adminstration</h1>
 		<table class="table table-hover">
-			<tr><th>Benutername</th><th>Passwort</th><th></th></tr>
+			<tr><th>Benutername</th><th>Letzer Login</th><th></th></tr>
   			<?php
-			foreach ($this->view['users'] as $userid=>$pass) {
+			foreach ($this->view['users'] as $userid=>$lastlogin) {
 				print   "<tr><td>$userid</td>" .
-						"<td><span onclick=\"$(this).html('$pass')\">******</span></td>" .
+						"<td>$lastlogin</td>" .
 						"<td class='text-right'><button type='button' class='btn btn-success''>Passwort ändern</button>" .
 						"&nbsp;<button type='button' class='btn btn-danger'>Löschen</button></td></tr>";	
 			}
 			?>
 		</table>
 		<button type="button" class="btn btn-success">Hinzufügen</button>
+		
