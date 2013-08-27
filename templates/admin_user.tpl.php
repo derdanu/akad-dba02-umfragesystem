eb1b5f68da1fad2ce05b67475becdb6fdbfdfa21
Daniel Falkner <bmw@740i.de>
2013-08-27 08:50:19 +0200

    Modal vorbereitung für Benutzeradmin

--- templates/admin_user.tpl.php
+++ templates/admin_user.tpl.php
@@ -5,10 +5,47 @@
 			foreach ($this->view['users'] as $userid=>$lastlogin) {
 				print   "<tr><td>$userid</td>" .
 						"<td>$lastlogin</td>" .
-						"<td class='text-right'><button type='button' class='btn btn-success''>Passwort ändern</button>" .
+						"<td class='text-right'><a data-toggle='modal' href='#changePassModal' class='btn btn-success'>Passwort ändern</a>" .
 						"&nbsp;<button type='button' class='btn btn-danger'>Löschen</button></td></tr>";	
 			}
 			?>
 		</table>
-		<button type="button" class="btn btn-success">Hinzufügen</button>
-		
+		<a data-toggle="modal" href="#addUserModal" class="btn btn-success">Hinzufügen</a>
+
+	  <!-- Modal -->
+	  <div class="modal" id="addUserModal">
+	    <div class="modal-dialog">
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+	          <h4 class="modal-title">Neuen Benutzer hinzufügen</h4>
+	        </div>
+	        <div class="modal-body">
+	          ...
+	        </div>
+	        <div class="modal-footer">
+	          <a href="#" class="btn" data-dismiss="modal">Verwerfen</a>
+	          <a href="#" class="btn btn-primary">Speichern</a>
+	        </div>
+	      </div><!-- /.modal-content -->
+	    </div><!-- /.modal-dialog -->
+	  </div><!-- /.modal -->
+	  
+	  <!-- Modal -->
+	  <div class="modal" id="changePassModal">
+	    <div class="modal-dialog">
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+	          <h4 class="modal-title">Passwort ändern</h4>
+	        </div>
+	        <div class="modal-body">
+	          ...
+	        </div>
+	        <div class="modal-footer">
+	          <a href="#" class="btn" data-dismiss="modal">Verwerfen</a>
+	          <a href="#" class="btn btn-primary">Speichern</a>
+	        </div>
+	      </div><!-- /.modal-content -->
+	    </div><!-- /.modal-dialog -->
+	  </div><!-- /.modal -->
