5e11dfcb9570211e68cf64715781f19621ecd7bf
Daniel Falkner <bmw@740i.de>
2013-08-28 14:02:01 +0200

    Doku

--- docs/html/search/all_64.js
+++ docs/html/search/all_64.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['database',['Database',['../class_database.html',1,'']]],
+  ['delete_5faction',['Delete_Action',['../class_controller_1_1_admin_1_1_user.html#a96952a534cf944ff774fdab36d8efaaa',1,'Controller::Admin::User']]],
   ['destroy',['destroy',['../class_session.html#a5dde74b6fa44649e5b73cb1096930dd4',1,'Session']]],
   ['display',['display',['../class_view.html#a0924100c5e1690a4150bb1c446859e07',1,'View']]]
 ];
