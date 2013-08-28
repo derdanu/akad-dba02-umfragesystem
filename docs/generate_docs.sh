1dc186ca9de50f3b8556c61a6c2e47f12979a704
Daniel Falkner <bmw@740i.de>
2013-08-28 13:29:30 +0200

    Doku

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 doxygen-1.8.5 doxygen.conf
+cd latex
+file=Makefile
+filedist=Makefile.dist
+cp $file $filedist
+head -$(expr $(cat $filedist | wc -l) - 1) $filedist > $file
+echo -e -n "\t" >> $file
+echo "rm -f *.ps *.dvi *.aux *.toc *.idx *.ind *.ilg *.log *.out *.brf *.blg *.bbl " >> $file
+make 	
+make clean
+cd ../latex_source
+make 
+make clean
+cd ..
