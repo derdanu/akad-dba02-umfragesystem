c22198ef5de3a0f7292ae8a13fd630a447c433c3
Daniel Falkner <bmw@740i.de>
2013-08-29 09:17:11 +0200

    Template bereinigt

--- templates/survey.tpl.php
+++ templates/survey.tpl.php
@@ -6,15 +6,13 @@
 			<h3 class="panel-title"><?php print $this->view['survey_name']; ?></h3>
 			</div>
 			<div class="panel-body">
-				<?php 
-				foreach ($this->view['survey_items'] as $arr) {
-					print 	"<div class='checkbox'>" .
-							"<label>" .
-							"<input type='checkbox' value='{$arr['ID']}' name='answer[]'>{$arr['Name']}" .
-							"</label>" .
-							"</div>";	
-				}
-				?>
+				<?php foreach ($this->view['survey_items'] as $arr) { ?>
+					<div class='checkbox'>
+						<label>
+							<input type="checkbox" value="<?php print $arr['ID']; ?>" name="answer[]"><?php print $arr['Name']; ?>
+						</label>
+					</div>	
+				<?php } ?>
 				<button type="submit" class="btn btn-primary">Absenden</button>
 			</div>
 		</div>
