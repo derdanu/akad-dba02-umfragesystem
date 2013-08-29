c296da18d8aadcc984011f0b47e1057d67c280dd
Daniel Falkner <bmw@740i.de>
2013-08-29 09:19:01 +0200

    Template bereinigt.

--- templates/surveys.tpl.php
+++ templates/surveys.tpl.php
@@ -2,10 +2,8 @@
       <div class="jumbotron">
         <h1>Bitte Umfrage ausw&auml;hlen</h1>
 		<ul class="list-group">
-			<?php 
-			foreach ($this->view['surveys'] as $arr) {
-				print "<li class='list-group-item'><a href='?controller=Survey&survey={$arr['ID']}''>{$arr['Name']}</a></li>";	
-			}
-			?>
+			<?php foreach ($this->view['surveys'] as $arr) { ?>
+				<li class="list-group-item"><a href="?controller=Survey&survey=<?php print $arr['ID'] ?>"><?php print $arr['Name'] ?></a></li>	
+			<?php } ?>
 		</ul>
       </div>
